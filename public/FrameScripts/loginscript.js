e507e6a28d4d11004743340469505b2c52e05113
Hindrik Taatgen <rik.taatgen@gmail.com>
2016-03-11 15:21:02 +0100

    Loginscreen

--- public/FrameScripts/loginscript.js
+++ public/FrameScripts/loginscript.js
@@ -1,3 +1,19 @@
 /**
  * Created by Rik on 11-3-2016.
  */
+angular.module('login', [])
+    .controller("loginController", function ($scope, $http) {
+
+        $scope.loginresult = "Awaiting login...";
+
+
+        $scope.loginsubmit = function () {
+            "use strict";
+            var sendData = angular.toJson({name: $scope.loginname, password: $scope.loginpassword});
+            $http.post("Login", sendData).then(function successCallback() {
+        
+            })
+            $scope.loginresult = "Logging in..."
+        }
+
+    })
