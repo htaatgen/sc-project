21669d70deb08dad557a071ab5d538810c6b18aa
Hindrik Taatgen <rik.taatgen@gmail.com>
2016-03-22 14:07:57 +0100

    Updating for Heroku

--- SCPServer.js
+++ SCPServer.js
@@ -229,6 +229,6 @@ io.on('connection', function (socket) {
     });
 })
 ;
-http.listen(3001, function () {
+http.listen(process.env.port || 3001, function () {
     console.log("Star Arena server activated.")
 });
